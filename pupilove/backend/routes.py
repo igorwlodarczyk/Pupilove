a072924ff71e64b134d359f88bbf16452a725d31
Igor Wlodarczyk <wlodar.igor@gmail.com>
2025-04-06 13:13:54 +0200

    frontend backend split

---
+++ pupilove/backend/routes.py
@@ -0,0 +1,8 @@
+from fastapi import APIRouter, Depends
+from database import get_db_connection
+from fastapi.responses import JSONResponse
+router = APIRouter()
+
+@router.get("/execute-select", response_class=JSONResponse)
+def query_input_form(query: str, db=Depends(get_db_connection)):
+    return db.execute_select(query)
--- pupilove/frontend/routes.py
+++ pupilove/frontend/routes.py
@@ -1,13 +1,15 @@
+import requests
+import starlette.status as status
 from pathlib import Path
-from fastapi.responses import HTMLResponse
-from fastapi import APIRouter, Depends, Request, Form
+from fastapi.responses import HTMLResponse, RedirectResponse
+from fastapi import APIRouter, Request, Form
 from fastapi.templating import Jinja2Templates
-from database import get_db_connection
+
 
 router = APIRouter()
 current_dir = Path(__file__).resolve().parent
 templates = Jinja2Templates(directory=current_dir / "templates")
-
+BASE_URL = "http://localhost:8000"
 
 @router.get("/")
 async def home(request: Request):
@@ -23,9 +25,14 @@ def query_input_form(request: Request):
 
 @router.post("/test-db-connection", response_class=HTMLResponse)
 def query_input(
-    request: Request, query: str = Form(...), db=Depends(get_db_connection)
+    request: Request, query: str = Form(...)
 ):
-    result = db.execute_select(query)
-    return templates.TemplateResponse(
-        "query_input.html", {"request": request, "result": result}
-    )
+    response = requests.get(f"{BASE_URL}/execute-select", params={"query": query})
+    if response.ok:
+        result = response.json()
+        return templates.TemplateResponse(
+            "query_input.html", {"request": request, "result": result}
+        )
+    else:
+        return RedirectResponse(url="/test-db-connection", status_code=status.HTTP_302_FOUND)
+
