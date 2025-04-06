d313645eec212547e25c2859489ac5a059f7e9fe
Igor Wlodarczyk <wlodar.igor@gmail.com>
2025-04-06 12:35:34 +0200

    script fix

--- scripts/run_mysql_db.sh
+++ scripts/run_mysql_db.sh
@@ -1,21 +1,22 @@
 #!/bin/bash
 
-# Ustawienie zmiennych środowiskowych
+# Setting environment variables
 export DB_IP="localhost"
 export DB_USER="pupilove_admin"
 export DB_PASSWORD="12345"
 export DB_NAME="pupilove"
 export DB_PORT="5001"
-export CONTAINER_NAME="pupilove_percona_mysql"
+DB_SCHEMA_FILE="scripts/db_schema.sql"
+SAMPLE_DATA_SQL_FILE="scripts/sample_data.sql"
 
-# Sprawdzanie, czy kontener o nazwie $CONTAINER_NAME już istnieje
+# Check if a container with the name $CONTAINER_NAME already exists
 if [ $(docker ps -aq -f name=$CONTAINER_NAME) ]; then
-  echo "Kontener $CONTAINER_NAME istnieje. Usuwam go..."
+  echo "Container $CONTAINER_NAME already exists. Removing it..."
   docker stop $CONTAINER_NAME
   docker rm $CONTAINER_NAME
 fi
 
-# Uruchomienie kontenera Percona MySQL
+# Start Percona MySQL container
 docker run -d \
   --platform linux/amd64 \
   --name $CONTAINER_NAME \
@@ -23,24 +24,21 @@ docker run -d \
   -p $DB_PORT:3306 \
   percona:latest
 
-# Czekanie aż kontener wystartuje
-echo "Czekam na uruchomienie kontenera..."
+# Wait for the container to start
+echo "Waiting for the container to start..."
 sleep 60
 
-# Tworzenie użytkownika i bazy danych
+# Create user and database
 docker exec -i $CONTAINER_NAME mysql -uroot -proot_password <<EOF
 CREATE DATABASE IF NOT EXISTS $DB_NAME;
 CREATE USER IF NOT EXISTS '$DB_USER'@'%' IDENTIFIED BY '$DB_PASSWORD';
 GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$DB_USER'@'%';
 FLUSH PRIVILEGES;
-USE $DB_NAME;
-CREATE TABLE IF NOT EXISTS test_table (
-    column1 VARCHAR(255),
-    column2 VARCHAR(255),
-    column3 VARCHAR(255)
-);
-INSERT INTO test_table (column1, column2, column3)
-VALUES ('Everything', 'is', 'working');
 EOF
 
-echo "Użytkownik $DB_USER stworzony i baza danych $DB_NAME utworzona."
+# Load database schema from db_schema.sql
+echo "Loading database schema from file $DB_SCHEMA_FILE..."
+docker exec -i $CONTAINER_NAME mysql -u$DB_USER -p$DB_PASSWORD $DB_NAME < "$DB_SCHEMA_FILE"
+docker exec -i $CONTAINER_NAME mysql -u$DB_USER -p$DB_PASSWORD $DB_NAME < "$SAMPLE_DATA_SQL_FILE"
+
+echo "User $DB_USER created and database $DB_NAME initialized using schema $DB_SCHEMA_FILE."
